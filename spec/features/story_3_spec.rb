b3d3c08a0c1d9a9626065ad87d0e822ef7430d00
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-19 10:15:23 -0700

    Remove story 3 readme

--- spec/features/story_3_spec.rb
+++ spec/features/story_3_spec.rb
@@ -1,10 +1,3 @@
-# As a visitor
-# When I visit a snack show page
-# I see the name of that snack
-#   and I see the price for that snack
-#   and I see a list of locations with vending machines that carry that snack
-#   and I see the average price for snacks in those vending machines
-#   and I see a count of the different kinds of items in that vending machine.
 require 'rails_helper'
 
 describe "User Story 3" do
