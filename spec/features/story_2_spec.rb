32a94450efba2e85175e1e83cc6ac13f94f3e21f
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-19 09:52:19 -0700

    Add spec for story 2

---
+++ spec/features/story_2_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "User Story 2:" do
+  it "vending machine show page show average price for snacks" do
+    jomah = Owner.create(name: "Jomah")
+    turing = Machine.create(location: "Turing", owner: jomah)
+    mm = Snack.create(name: "M&Ms", price: 1_00)
+    sunchips = Snack.create(name: "Sunchips", price: 1_25)
+    jerky = Snack.create(name: "Jack Links", price: 1_75)
+    turing.snacks << [mm, sunchips, jerky]
+
+    visit "/machine/#{turing.id}"
+    expect(page).to have_content 133
+  end
+end
