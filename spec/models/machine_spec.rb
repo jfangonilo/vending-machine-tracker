7562a541458e60d2e502e2134a8b5e3d6bf15fe3
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-19 09:52:02 -0700

    Add machine spec and method for average snackprice

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Machine, type: :model do
     it {should have_many :machine_snacks }
     it {should have_many(:snacks).through(:machine_snacks)}
   end
+
+  describe 'methods' do
+    it "average snack price" do
+      jomah = Owner.create(name: "Jomah")
+      turing = Machine.create(location: "Turing", owner: jomah)
+      mm = Snack.create(name: "M&Ms", price: 1_00)
+      sunchips = Snack.create(name: "Sunchips", price: 1_25)
+      jerky = Snack.create(name: "Jack Links", price: 1_75)
+      turing.snacks << [mm, sunchips, jerky]
+
+      expect(turing.average_snack_price).to be_between(133, 134)
+    end
+  end
 end
